4fc3041302f146683a65f763b9a297e3bb207402
andriatsiory hasiniaina <andriatsiory.hasiniaina.ext@up.coop>
2024-04-08 17:21:28 +0200

    add scaleup

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# autoscale-azure-app-service-plan
-Scale Up and down 
+# Scale Up and Down App Service Plan
+
+This script is designed to scale up and Down Azure App Service Plans in multiple subscriptions and resource groups.
+
+## Configuration
+
+The script takes the following parameters:
+
+- `ARM_TENANT_ID`: The Azure AD tenant ID for authentication.
+- `ARM_CLIENT_ID`: The client ID of the service principal for authentication.
+- `ARM_CLIENT_SECRET`: The client secret of the service principal for authentication.
+- `Subscriptions`: An array of subscription objects containing subscription ID, resource groups, and App Service plans to be scaled up.
+
+## Usage
+
+You can use the script by providing the necessary parameters and executing it. It will then iterate through the specified subscriptions and resource groups to scale up the specified App Service Plans.
+
+## Functions
+
+### `ScaleUpAppServicePlan`
+
+This function takes the resource group name, App Service Plan name, desired size, and subscription ID as parameters. It uses Azure CLI commands to check and scale up the App Service Plan if necessary.
+
+### Main Script
+
+The script connects to Azure using the specified service principal, iterates through the subscriptions and resource groups, and scales up the specified App Service Plans.
---
+++ scaleup.ps1
@@ -0,0 +1,88 @@
+
+Param(
+    $ARM_TENANT_ID = "x",    
+    $ARM_CLIENT_ID = "x",    
+    $ARM_CLIENT_SECRET = 'x',
+    $Subscriptions = @(
+        @{
+            #Subscription 1
+            "SubscriptionId" = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx"
+            "ResourceGroups" = @(
+                @{
+                    "ResourceGroupName" = "rg-01"
+                    "AppServicePlanNames"     = @("asp01")
+                    "SizeUp" = "B2"
+                }
+            )
+        }
+        @{
+            #Subscription 2
+            "SubscriptionId" = "xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx"
+            "ResourceGroups" = @(
+                @{
+                    "ResourceGroupName" = "rg-02"
+                    "AppServicePlanNames"     = @("asp02")
+                    "SizeUp" = "S2"
+                },
+                @{
+                    "ResourceGroupName" = "rg-03"
+                    "AppServicePlanNames"     = @("asp031","asp032")
+                    "SizeUp" = "S2"
+                }
+            )
+        }
+    )
+) #EndParam
+function ScaleUpAppServicePlan($resourceGroupName, $AppServicePlanName,$sizeUps, $subscriptionId) {
+    # $server = Get-AzAppServicePlan -ResourceGroupName $resourceGroupName -Name $AppServicePlanName -SubscriptionId $subscriptionId 
+    # $server = az appservice plan show --resource-group $resourceGroupName --name $AppServicePlanName --subscription $subscriptionId --output yaml
+    # az appservice plan show --resource-group $resourceGroupName --name $AppServicePlanName --subscription $subscriptionId --output jsonc --query "sku.size"
+     $server = az appservice plan show --resource-group $resourceGroupName --name $AppServicePlanName --subscription $subscriptionId 
+    #  Write-Host "$server"
+     if (!$server) {
+        Write-Output "`t`t`t App Service Plan $AppServicePlanName not found in resource group  $resourceGroupName"
+        return
+    }
+    $serverSize = az appservice plan show --resource-group $resourceGroupName --name $AppServicePlanName --subscription $subscriptionId --output jsonc --query "sku.size" | ConvertFrom-Json
+    Write-Host "$serverSize : $sizeUps"
+    if ($serverSize -eq $sizeUps) {
+        Write-Output "`t`t`tThe App Service Plan $AppServicePlanName is already with this size $sizeUps "
+        return
+    }
+    else {
+        $scaleUpCommand = az appservice plan update --resource-group $resourceGroupName --name $AppServicePlanName --subscription $subscriptionId --sku $sizeUps
+        if ($?) {
+            Write-Output "`t`t`tScaleUp App Service Plan $AppServicePlanName in resource group $resourceGroupName to $sizeUps"
+        } else {
+            Write-Output "An error occurred while scaling up App Service Plan $AppServicePlanName in resource group $resourceGroupName"
+        }
+    }
+}
+
+# Checkout in Subscription
+foreach ($subscription in $Subscriptions) {
+    $subscriptionId = $subscription.SubscriptionId
+    $resourceGroups = $subscription.ResourceGroups
+
+    # Authentification using SP
+    $SecureStringPwd = $ARM_CLIENT_SECRET | ConvertTo-SecureString -AsPlainText -Force
+    $pscredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ARM_CLIENT_ID, $SecureStringPwd
+    Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $ARM_TENANT_ID
+    Set-AzContext -SubscriptionId $subscriptionId
+
+    Write-Output "Processing resources in subscription $subscriptionId"
+
+    foreach ($resourceGroup in $resourceGroups) {
+        $resourceGroupName = $resourceGroup.ResourceGroupName
+        $AppServicePlanNames = $resourceGroup.AppServicePlanNames
+        $sizeUps             = $resourceGroup.SizeUp
+
+        Write-Output "`tProcessing resources in resource group $resourceGroupName"
+
+        # ScaleUp each AppServicePlanNames
+        foreach ($AppServicePlanName in $AppServicePlanNames) {
+            Write-Output "`t`tProcessing App Service Plan  $AppServicePlanName"
+            ScaleUpAppServicePlan $resourceGroupName $AppServicePlanName $sizeUps $subscriptionId
+        }
+    }
+}
